c9e3529a62d7aebc8f5fb2018062dd5dbbe5d89f
hangieun <gksrldms1120@naver.com>
2024-06-23 22:20:52 +0900

    modify all

--- 2.air purifier/js/main.js
+++ 2.air purifier/js/main.js
@@ -8,11 +8,41 @@ $(document).ready(function(){
       });
 
     
+    
     var swiper = new Swiper(".productStation", {
     slidesPerView: 2.5,
     spaceBetween: 30,
     centeredSlides: true,
     });
+
+    // 텍스트 효과
+    $(window).scroll(function(){
+      let scrTop = 0
+      let devHeight = $(window).height()
+      scrTop = $(this).scrollTop()
+      $(".ctrlTxt").each(function(){
+          if(scrTop > $(this).offset().top - devHeight*0.7){
+              $(this).addClass("on")    
+          }else{$(this).removeClass("on")}
+      })
+  })
+
+  $(window).scroll(function(){
+    let scrTop = 0
+    let devHeight = $(window).height()
+    scrTop = $(this).scrollTop()
+    $(".serviceSection").each(function(){
+        if(scrTop > $(this).offset().top - devHeight*0.7){
+            $(this).addClass("on")    
+        }else{$(this).removeClass("on")}
+    })
+})
+
+
+  $(".supportMenu>dt").click(function(){
+    $(".supportMenu>dt").removeClass("on")
+    $(this).addClass("on")
+  })
 
 
 
