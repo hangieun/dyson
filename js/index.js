9492df7c7987f57b89b09aaccbc89ebcd732e2e0
hangieun <gksrldms1120@naver.com>
2024-06-28 18:10:28 +0900

    dyson index

---
+++ js/index.js
@@ -0,0 +1,73 @@
+$(function(){
+
+    // 2섹션 슬라이드
+    var swiper = new Swiper(".productStation", {
+     
+        breakpoints: {
+              320: {
+                  slidesPerView: 1.2,
+                  spaceBetween: 10
+              },
+              768: {
+                  slidesPerView: 2.5,
+                  spaceBetween: 15
+              },
+            
+              1024: {
+                  slidesPerView: 3.2,
+                  spaceBetween: 20
+              }
+          }
+    
+    });
+
+    // 3섹션 슬라이드
+    var swiper = new Swiper(".priceStation", {
+        breakpoints: {
+          320: {
+              slidesPerView: 1.2,
+              spaceBetween: 10
+          },
+          768: {
+              slidesPerView: 2.5,
+              spaceBetween: 15
+          },
+        
+          1024: {
+              slidesPerView: 3.2,
+              spaceBetween: 20
+          }
+      }
+    });
+
+    //  4섹션 슬라이드
+    let promoCount = 0
+    $(".nextBtn").click(function(){
+        promoCount++
+        if(promoCount>3){promoCount=3}
+        $(".promoTrain").css("transform",`translateX(${-25*promoCount}%)`)
+    })
+    $(".prevBtn").click(function(){
+        promoCount--
+        if(promoCount<0){promoCount=0}
+        $(".promoTrain").css("transform",`translateX(${-25*promoCount}%)`)
+    })
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
