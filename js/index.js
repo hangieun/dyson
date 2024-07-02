464d6042b0f067c2e5a98aa594d865772359edba
hangieun <gksrldms1120@naver.com>
2024-07-02 14:53:14 +0900

    index mobile

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $(function(){
      
         breakpoints: {
               320: {
-                  slidesPerView: 1.2,
+                  slidesPerView: 1.3,
                   spaceBetween: 10
               },
               768: {
@@ -25,7 +25,7 @@ $(function(){
     var swiper = new Swiper(".priceStation", {
         breakpoints: {
           320: {
-              slidesPerView: 1.2,
+              slidesPerView: 1.3,
               spaceBetween: 10
           },
           768: {
